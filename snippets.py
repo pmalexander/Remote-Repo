1f85756cbe02af3d3193069627b0cfa092f4938d
Patrick Alexander Jr <pmalexander5@gmail.com>
2017-07-28 05:03:51 +0000

    Joining functions, added unpacking

--- snippets.py
+++ snippets.py
@@ -42,6 +42,16 @@ def main():
     get_subparser.add_argument("snippet", help="Snippet text")
     
     arguments = parser.parse_args()
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {!r}".format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
 
 if __name__ == "__main__":
     main()
