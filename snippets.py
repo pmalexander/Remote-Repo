c339cff3b93c5f915fa6df0627af1851fccbb1dc
Patrick Alexander Jr <pmalexander5@gmail.com>
2017-07-28 05:21:14 +0000

    Removed argument from get function

--- snippets.py
+++ snippets.py
@@ -37,9 +37,8 @@ def main():
     
     # Subparser for the get command
     logging.debug("Constructs the get subparser")
-    get_subparser = subparsers.add_parser("get", help="Retrieves a snippet")
+    get_subparser = subparsers.ad_parser("get", help="Retrieves a snippet")
     get_subparser.add_argument("name", help="Name of the snippet")
-    get_subparser.add_argument("snippet", help="Snippet text")
     
     arguments = parser.parse_args()
     # Convert parsed arguments from Namespace to dictionary
