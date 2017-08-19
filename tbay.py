828360b3bbc623a8f1d3b6714b825ef279db999c
Patrick Alexander Jr <pmalexander5@gmail.com>
2017-08-19 19:01:58 +0000

    newest addition

--- tbay.py
+++ tbay.py
@@ -8,29 +8,30 @@ session = Session()
 Base = declarative_base()
 
 from datetime import datetime
-
 from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey, desc
 from sqlalchemy.orm import relationship
 
 class Item(Base):
     __tablename__ = "items"
-
+    
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
     owner_id = Column(Integer, ForeignKey(user.id, nullable=False)
     
-    user_bid = relationship("Bid", backref="bidfor_item")
+    bidded_item = relationship("Bid", backref="bidded_item")
     
-
 class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
-    bid = relationship("Bid", uselist=True, backref="User")
+
+#sets relationship with the items and the users as bidders for items    
+    auc_item = relationship("Item", backref="auc_item")
+    auc_bid = relationship("Bid", backref="auc_bidder")
     
 class Bid(Base):
     __tablename__ = "bids"
@@ -39,9 +40,10 @@ class Bid(Base):
     price = Column(Float, nullable=False)
     item_id = Column(Integer, ForeignKey('Item.id'), nullable=False)
     user_id = Column(Integer, ForeignKey('User.id'), nullable=False)
-    
+
 Base.metadata.create_all(engine)
 
+#sets usernames and passwords for each bidder
 jack = User()
 jack.username = "jbaker"
 jack.password = "chainsaw"
@@ -63,6 +65,7 @@ luke.password = "cage"
 session.add(luke)
 session.commit()
 
+#sets the items, names, and descriptions
 axe = Item()
 axe.name = "Axe"
 axe.description = "Huge"
@@ -113,27 +116,45 @@ axe = session.query(Item).filter(Item.name == 'Axe').first()
 lantern = session.query(Item).filter(Item.name == 'Lantern').first()
 remote = session.query(Item).filter(Item.name == 'Remote').first()
 rope = session.query(Item).filter(Item.name == 'Remote').first()
-wheelchair = session.query(Item).filter(Item.name == 'Wheelchair').first()
+wheelchair = session.query(Item.filter(Item.name == 'Wheelchair').first()
 baseball = session.query(Item).filter(Item.name == 'Baseball').first()
+
+all_bids = session.query(Bid).all()
+
+#identifies Jack Baker as the owner of the baseball
+baseball = Item(baseball.name="baseball", baseball.description="Round", baseball.owner=jack)
+session.add(baseball)
+session.commit()
+print("{} auctions for {} on {}".format(baseball.owner.username, baseball.name, baseball.start_time))
+
+#identifies both Marguerite and Luke Baker as bidders for the baseball
+m_bid = Bid(price = 100, item_id=baseball.id, auc_bidder=marguerite)
+l_bid = Bid(price = 200, item_id=baseball.id, auc_bidder=luke)
+
+for bid in all_bids
+    item = session.query(Item).filter(Item.id == bid.item_id).first()
+    ouruser = session.query(User).filter(User.id == bid._bidder_id).first()
+    print("price:{}, bidder:{}, item:{}".format(bid.price, user.name, item.name))
+
+# returns the item_id and description for all baseballs which were created in the past.  
+# import the datetime object: from datetime import datetime
+session.query(Item.id, Item.description).filter(Item.name == "baseball", Item.start_time < datetime.utcnow()).all()
+test = session.query(Item).filter(Item.name == "baseball").first()
+session.query(Bid).filter(Item.id == "baseball").order_by(desc(Bid.price))
+
+# Returns the description of all of the basesball
+#session.query(Item.description).filter(Item.name == "baseball").all()
 
 # Returns a list of all of the user objects
 # Note that user objects won't display very prettily by default -
 # you'll see their type (User) and their internal identifiers.
-session.query(User).all() # Returns a list of all of the user objects
+#session.query(User).all() # Returns a list of all of the user objects
 
 # Returns the first user
-session.query(User).first()
+#session.query(User).first()
 
 # Finds the user with the primary key equal to 1
-session.query(User).get(1)
+#session.query(User).get(1)
 
 # Returns a list of all of the usernames in ascending order
-session.query(User.username).order_by(User.username).all()
-
-# Returns the description of all of the basesballs
-session.query(Item.description).filter(Item.name == "baseball").all()
-
-# Return the item id and description for all baseballs which were created in the past.  Remember to import the datetime object: from datetime import datetime
-session.query(Item.id, Item.description).filter(Item.name == "baseball", Item.start_time < datetime.utcnow()).all()
-test = session.query(Item).filter(Item.name == "baseball").first()
-session.query(Bid).filter(Item.id == "baseball").order_by(desc(Bid.price))
+# session.query(User.username).order_by(User.username).all()
